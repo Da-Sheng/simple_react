2c97a29f7ab72ad484678a981e304d0f41d1c159
sk <dashengzs1314@gmail.com>
2025-07-27 19:37:11 +0800

    Fiber

--- react-dom/client.js
+++ react-dom/client.js
@@ -1,26 +1,99 @@
-function render(element, container) {
-    const dom = 
-        element.type === 'TEXT_ELEMENT' ? document.createTextNode('') : document.createElement(element.type);
+let nextUnitOfWork = null;
 
+function render(element, container) {
+    nextUnitOfWork = {
+        dom: container,
+        props: {
+            children: [element]
+        }
+    }
+}
+
+const createDom = (fiber) => {
+    const dom = fiber.type === 'TEXT_ELEMENT' ? document.createTextNode('') : document.createElement(fiber.type);
     const isProperty = name => name !== 'children' && !name.startsWith('on');
     const isEvent = name => name.startsWith('on');
 
-    Object.keys(element.props).forEach(name => {
+    Object.keys(fiber.props).forEach(name => {
         if (isProperty(name)) {
-            dom[name] = element.props[name];
+            dom[name] = fiber.props[name];
         }
         if (isEvent(name)) {
             const eventType = name.toLowerCase().substring(2);
-            dom.addEventListener(eventType, element.props[name]);
+            dom.addEventListener(eventType, fiber.props[name]);
         }
     });
 
-    element.props.children.forEach(child => {
-        render(child, dom);
-    });
-
-    container.appendChild(dom);
+    return dom;
 }
+
+function workLoop(deadline) {
+    let shouldYield = false;
+    while (nextUnitOfWork && !shouldYield) {
+        nextUnitOfWork = performUnitOfWork(nextUnitOfWork);
+        shouldYield = deadline.timeRemaining() < 1;
+    }
+    requestIdleCallback(workLoop);
+}
+// 执行单元任务 执行一个单元任务, 并返回下一个单元任务
+function performUnitOfWork(fiber) {
+    // 创建dom
+    if (!fiber.dom) {
+        fiber.dom = createDom(fiber);
+    }
+    // 将dom添加到父元素
+    if (fiber.parent) {
+        fiber.parent.dom.appendChild(fiber.dom);
+    }
+    // 遍历子元素
+    const elements = fiber.props.children;
+    let index = 0;
+    let prevSibling = null;
+
+    while (index < elements.length) {
+        const element = elements[index];
+        // 创建新的fiber
+        const newFiber = {
+            type: element.type,
+            props: element.props,
+            // 父元素
+            parent: fiber,
+            dom: null
+        }
+        // 第一个子元素
+        if (index === 0) {
+            fiber.child = newFiber;
+        } else {
+            // 其他子元素
+            prevSibling.sibling = newFiber;
+        }
+        prevSibling = newFiber;
+        index++;
+    }
+    // 如果有子元素，则返回子元素
+    if (fiber.child) {
+        return fiber.child;
+    }
+    // 如果没有子元素，则返回兄弟元素
+    let nextFiber = fiber;
+    while (nextFiber) {
+        // 如果有兄弟元素，则返回兄弟元素
+        if (nextFiber.sibling) {
+            return nextFiber.sibling;
+        }
+        // 如果没有兄弟元素, 则往上找父元素
+        nextFiber = nextFiber.parent;
+    }
+    // 如果没有, 则返回null
+    return null;
+}
+
+// function commitRoot() {
+//     commitWork(nextUnitOfWork);
+//     nextUnitOfWork = null;
+// }
+
+requestIdleCallback(workLoop);
 
 export default {
     render
